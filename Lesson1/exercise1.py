90cfbf7ff5582f7608500622a1985cc8f175b928
Kyle Ringler <92175614+linuxpy76@users.noreply.github.com>
2025-07-22 16:04:17 -0500

    Create exercise1.py

---
+++ Lesson1/exercise1.py
@@ -0,0 +1,9 @@
+sf_gw1 = "172.31.255.1/24"
+sf_gw2 = input("What is the IP/CIDR for GW 2?: ")
+header = "-" * 20
+
+print()
+print(f"{'sf_gw1':^20} {'sf_gw2':^20}")
+print(f"{header:^20} {header:^20}")
+print(f"{sf_gw1:^20} {sf_gw2:^20}")
+print()
