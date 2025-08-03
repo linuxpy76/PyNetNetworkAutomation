6029193d6045f284a9adf989157f09122c46d845
Kyle Ringler <92175614+linuxpy76@users.noreply.github.com>
2025-08-03 00:06:59 -0500

    Update exercise1.py

--- Lesson4/exercise1.py
+++ Lesson4/exercise1.py
@@ -1,14 +1,17 @@
+import re
+
+ipv4_pattern = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+
 with open("show_ip_int_brief.txt", "r") as f:
     data = f.readlines()
 
 ip_addresses = {}
 
 for line in data:
-    if "10." in line:
-        line_split = line.split()
-        interface = line_split[0]
-        ip_address = line_split[1]
-        ip_addresses.update({interface: ip_address})
+    if re.match(ipv4_pattern, line.split()[1]):
+        interface = line.split()[0]
+        ip_address = line.split()[1]
+        ip_addresses[interface] = ip_address
 
 for k, v in ip_addresses.items():
     print(f"{k} --> {v}")
