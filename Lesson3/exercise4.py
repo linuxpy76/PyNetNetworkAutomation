27fc73bfbd346daac19667e4e82adc100e609ed7
Kyle Ringler <92175614+linuxpy76@users.noreply.github.com>
2025-08-04 08:18:18 -0500

    Update exercise4.py

--- Lesson3/exercise4.py
+++ Lesson3/exercise4.py
@@ -1,3 +1,7 @@
+import re
+
+ipv4_pattern = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+
 header = "-" * 20
 
 with open("show_ip_int_brief.txt", "r") as f:
@@ -8,10 +12,9 @@ intf_name_list = []
 ip_addr_list = []
 
 for line in show_ip:
-    if "10." in line:
-        ip_line = line.split()
-        intf_name_list.append(ip_line[0])
-        ip_addr_list.append(ip_line[1])
+    if re.match(ipv4_pattern, line.split()[1]):
+        intf_name_list.append(line.split()[0])
+        ip_addr_list.append(line.split()[1])
 
 print("Interface and IP address lists:")
 print(intf_name_list)
