76c943b0502eafe57c8286f021b1d06123b27b02
Kyle Ringler <92175614+linuxpy76@users.noreply.github.com>
2025-08-05 19:16:16 -0500

    Update exercise4.py

--- Lesson4/exercise4.py
+++ Lesson4/exercise4.py
@@ -1,29 +1,31 @@
 from rich import print
 
 # Read in file
-with open("arubacx_show_vlan.txt") as f:
+filename = "arubacx_show_vlan.txt"
+
+with open(filename) as f:
     data = f.read()
 
-# Initialize blank directory
+# Initialize blank dictionary
 vlan_map = {}
-
 for line in data.splitlines():
-    if "-----" in line:
+    # Skip header lines
+    if "----" in line:
         continue
-    if "Name" and "Type" in line:
+    if "Status" in line and "Reason" in line:
         continue
 
     vlan_id, vlan_name, status, reason, vlan_type, interfaces = line.split()
     intf_groups = interfaces.split(",")
 
+    # Construct list of interfaces associated with given VLAN-ID
     intf_list = []
-
-    # Construct list of interfaces associated with given VLAN ID
     for intf in intf_groups:
         # Check for ranges
         if "-" in intf:
             intf_start, intf_end = intf.split("-")
-            # Drop last character for base interface
+
+            # Dropping the last character will give us the base interface
             base_intf = intf_start[:-1]
 
             # Interfaces with x/x/x notation
@@ -33,13 +35,13 @@ for line in data.splitlines():
 
                 intf_fields = intf_end.split("/")
                 end_idx = int(intf_fields[-1])
-            
+
             # lag interfaces
             if "lag" in intf_start and "lag" in intf_end:
                 start_idx = int(intf_start.split("lag")[-1])
                 end_idx = int(intf_end.split("lag")[-1])
 
-            # Deconstruct the ranges
+            # Deconstruct the ranges i.e. each individual interface listed
             new_intf = [f"{base_intf}{idx}" for idx in range(start_idx, end_idx + 1)]
             intf_list = intf_list + new_intf
 
@@ -47,7 +49,7 @@ for line in data.splitlines():
             # not an interface range, just add the intf to the list
             intf_list.append(intf)
 
-    # Add intf_list to VLAN map
+    # intf_list is now fully constructed, add it to the VLAN map.
     vlan_map[int(vlan_id)] = intf_list
 
 print()
