8903bbeb96a9dde1008b8a3f4739e58181a75182
Kyle Ringler <92175614+linuxpy76@users.noreply.github.com>
2025-10-10 08:17:29 -0500

    Create exercise3.py

---
+++ Lesson5/exercise3.py
@@ -0,0 +1,16 @@
+import re
+
+with open("arista_show_ip_arp.txt", mode="r") as f:
+    data = f.read()
+
+ip_address = r"(\d+\.\d+\.\d+\.\d+)"
+mac_address = r"(\w+\.\w+\.\w+)"
+
+arp_table = re.findall(rf"^{ip_address}.+?{mac_address}.+?$", data, flags=re.MULTILINE)
+
+# print(arp_table)
+# print(dict(arp_table))
+arp_table = dict(arp_table)
+
+for k, v in arp_table.items():
+    print(k, v)
